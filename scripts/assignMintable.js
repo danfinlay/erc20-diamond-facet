977a7e2e2a658af2b0f02fce37e11b22f7fc0ec1
Dan Finlay <dan@danfinlay.com>
2021-12-29 19:48:31 -0800

    Assign mintable for issuing grants

--- scripts/assignMintable.js
+++ scripts/assignMintable.js
@@ -1,7 +1,8 @@
 /* global ethers */
 /* eslint prefer-const: "off" */
-const DIAMOND_ADDRESS = '0xe20f1ADE8B176F76E8D403d67E7D1512348E6fAB'
-const MINTABLE_ADDRESS = '0xE84988f1bb0c18Bb7C24FE3d2Aa26f8c51e1224e'
+const DIAMOND_ADDRESS = '0xf824a5f172BA61A050eBd166391a20932e036513'
+const MINTABLE_ADDRESS = '0xed9A3271c8Bf0133aba5585c96251e0884C94f9D'
+const grants = require('../grants.json')
 
 const { getSelectors, FacetCutAction } = require('./libraries/diamond.js')
 
@@ -9,31 +10,14 @@ async function deployDiamond () {
   const accounts = await ethers.getSigners()
   const contractOwner = accounts[0]
 
-  // Diamond
-  const diamond = await ethers.getContractAt('Diamond', DIAMOND_ADDRESS)
-  const mintable = await ethers.getContractAt('ERC20MintableFacet', MINTABLE_ADDRESS)
+  const mintable = await ethers.getContractAt('ERC20MintableFacet', DIAMOND_ADDRESS)
+  const amount = ethers.utils.parseEther('10000').toHexString();
 
-  const cut = []
-  cut.push({
-    facetAddress: MINTABLE_ADDRESS,
-    action: FacetCutAction.Add,
-    functionSelectors: ['0x40c10f19'] // mint()
-  })
-
-  // upgrade diamond with facets
-  console.log('')
-  console.log('Diamond Cut:', cut)
-  const diamondCut = await ethers.getContractAt('IDiamondCut', diamond.address)
-  let tx
-  let receipt
-  tx = await diamondCut.diamondCut(cut, MINTABLE_ADDRESS, '0x')
-  console.log('Diamond cut tx: ', tx.hash)
-  receipt = await tx.wait()
-  if (!receipt.status) {
-    throw Error(`Diamond upgrade failed: ${tx.hash}`)
+  for await (const grantee of grants) {
+    console.log(`Granting to ${grantee}`)
+    await mintable.mint(grantee, amount)
+    console.log(`Granted to ${grantee}`)
   }
-  console.log('Completed diamond cut')
-
   return diamond.address
 }
 
