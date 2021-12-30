6ad36d35ae27f35368440b5886f703a3f198d98d
Dan Finlay <dan@danfinlay.com>
2021-12-29 19:53:23 -0800

    Add batch minting to batch facet

--- contracts/facets/ERC20BatchFacet.sol
+++ contracts/facets/ERC20BatchFacet.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import { ERC20BaseInternal } from "../solidstate-solidity/token/ERC20/base/ERC20BaseInternal.sol";
+import { LibDiamond } from "../libraries/LibDiamond.sol";
 
 contract ERC20BatchFacet is ERC20BaseInternal {
 
@@ -11,4 +12,13 @@ contract ERC20BatchFacet is ERC20BaseInternal {
             _transfer(msg.sender, recipients[i], amounts[i]);
         }
     }
+
+    function batchMint(address[] calldata recipients, uint256[] calldata amounts) external {
+        LibDiamond.enforceIsContractOwner();
+        require(recipients.length == amounts.length, "Recipients length different than amounts");
+        for(uint256 i; i < recipients.length; i++) {
+            _mint(recipients[i], amounts[i]);
+        }
+    }
+
 }
